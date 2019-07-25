93267d8b51b4da032701bd2490113f318fdc3bcf
Jemsheer K D <jemsheer.k@thinkpalm.com>
2019-07-25 21:42:13 +0530

    Added Const in Loading Widget

--- lib/src/ui/widgets/LoadingWidget.dart
+++ lib/src/ui/widgets/LoadingWidget.dart
@@ -3,33 +3,38 @@ import 'package:Orchid/src/styles.dart';
 import 'package:flutter/material.dart';
 
 class LoadingWidget extends StatelessWidget {
+  final Color textColor;
+
+  const LoadingWidget({this.textColor = ColorConstant.WHITE});
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.vertical,
       child: Padding(
-          padding: EdgeInsets.fromLTRB(0, 80, 0, 0),
-          child: Center(
-            child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircularProgressIndicator(
-                    backgroundColor: ColorConstant.BLACK,
-                    valueColor:
-                        new AlwaysStoppedAnimation(ColorConstant.WHITE),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(50),
-                    child: Text(
-                      'Please Wait...',
-                      style: Styles.getWhiteTextTheme(
-                          Theme.of(context).textTheme.display1),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ]),
-          )),
+        padding: EdgeInsets.fromLTRB(0, 80, 0, 0),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                backgroundColor: ColorConstant.BLACK,
+                valueColor: new AlwaysStoppedAnimation(ColorConstant.WHITE),
+              ),
+              Padding(
+                padding: EdgeInsets.all(50),
+                child: Text(
+                  'Please Wait...',
+                  style: Styles.getColoredTextTheme(
+                      Theme.of(context).textTheme.display1, textColor),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
